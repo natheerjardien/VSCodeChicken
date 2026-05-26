05667a1c417025d9f74ef5d01054a42e1d9362d8
natheerjardien <st10435542@vcconnect.edu.za>
2026-05-26 11:51:20 +0200

    push

--- DockerChicken-master/ChickenAPI/Model/FarmDbContext.cs
+++ DockerChicken-master/ChickenAPI/Model/FarmDbContext.cs
@@ -9,6 +9,23 @@ namespace ChickenAPI.Model
 
         public FarmDbContext(DbContextOptions<FarmDbContext> options) : base(options) { }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // This ensures that when SQL Server is still executing the init-database script, 
+                // the API waits and retries instead of crashing immediately.
+                optionsBuilder.UseSqlServer(options => 
+                    options.EnableRetryOnFailure(
+                        maxRetryCount: 5,
+                        maxRetryDelay: TimeSpan.FromSeconds(5),
+                        errorNumbersToAdd: null
+                    )
+                );
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Chicken>()
